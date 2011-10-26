6b4c7aff08554d447b7f7a3c16504a8cd612873c
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2011-10-26 11:40:19 +0200

    Felhasználónév beállítva a subrepoknál

--- Scripts/ConfirmLeave.js
+++ Scripts/ConfirmLeave.js
@@ -7,15 +7,22 @@
                 var input = $("#" + fieldId);
                 var form = input.length > 0 ? $(input[0].form) : $();
 
+                var that = this;
+
                 // Detect changes on input elements
                 form.change(function () {
-                    alert("kjjjJ+");
+                    that.editorHasChanged = true;
                 });
 
                 // Detect also on textareas
                 form.find("textarea").change(function () {
                     alert("ddddddddddddd4");
                 });
+
+                window.onbeforeunload = function () {
+                    if (that.editorHasChanged)
+                        return "Do";
+                };
             }
         }
     });
